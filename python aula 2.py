3b526f65579fb537f2e1c364496bd45bcb2d4fd7
vitinho2100 <vitor08moraes@gmail.com>
2025-05-22 17:40:58 -0300

    Add files via upload

---
+++ python aula 2.py
@@ -0,0 +1,8 @@
+#  soma de 2
+n1 = float (input("numero 1 :"))
+n2 = float (input("numero 2  :"))
+calculo = n1 + n2
+print (" resultado +", n1 + n2)
+print (" resultado - ", n1 - n2)
+print (" resultado * ", n1 * n2)
+print (" resultado / ", n1 / n2)
