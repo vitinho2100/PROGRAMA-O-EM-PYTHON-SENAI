52825440915bcec3c8b980997fbbf195f422fb06
vitinho2100 <vitor08moraes@gmail.com>
2025-05-23 17:55:05 -0300

    Add files via upload

---
+++ Atividade_aula_5.py
@@ -0,0 +1,33 @@
+#1
+print(" olá seja bem vindo!")
+#2
+verdadeiro = True
+print(type(verdadeiro))
+#3
+print(10.5 * 2.00)
+#4
+print(10/2)
+#5
+print(10 - 5 )
+#6
+print(10/2)
+#7
+print(10.0 * 5.0 * 2.6 * 8.0)
+#8
+numero = 18
+print(numero ** 2)
+#9
+# nome idade emeil curso e endereço
+nome = input("digite o seu nome:")
+idade = int(input("digite a sua idade:"))
+emeil = input("digite o seu emeil:")
+endereço = input("digite o seu endereço:")
+curso = input("digite o seu curso:")
+
+print(f"""
+nome:{nome}
+idade :{idade}
+e-meil: {emeil}
+endereço:{endereço}
+curso : {curso}
+""")
