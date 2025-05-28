3bdfe7ad6fe01ae452dbb185cc7c7f8d31a43fdc
vitinho2100 <vitor08moraes@gmail.com>
2025-05-28 16:48:07 -0300

    Add files via upload

---
+++ exercícios.aula6.py
@@ -0,0 +1,113 @@
+# ATIVIDADE 1
+n1 = 4
+n2 = 6
+n3 = 2
+
+resultado = (n1 * n2) / n3
+print("resultado", resultado)
+
+# ATIVIDADE 2
+n1 = 5
+n2 = 7
+resultado = n1 + n2
+print("resultado",resultado)
+
+# ATIVIDADE 3
+n1 = 3
+n2 = 4
+resultado = n1**n2
+print("resultado",resultado)
+
+# ATIVIDADE 4
+n1 = 8
+n2 = 12
+n3 = 15
+n4 = 3
+resultado = (n1 + n2 + n3)/n4
+print("resultado", resultado)
+
+# ATIVIDADE 5
+n1 = 2
+n2 = 10
+n3 = 5
+resultado = (n1 - n2)*5
+print("resultado",resultado)
+
+# ATIVIDADE 6
+n1 = 27
+n2 = 3
+n3 = 5
+resultado = (27/3)+5
+print("resultado",resultado)
+
+# ATIVIDADE 7
+n1 = 17
+n2 = 4
+resultado = (17%4)
+print("resultado", resultado)
+
+# ATIVIDADE 8
+n1 = 9
+n2 = 3
+n3 = 2
+resultado = (9*3)**2
+print("resultado",resultado)
+
+# ATIVIDADE 9
+n1 = 81
+n2 = 0,5
+resultado = (81**0,5)
+print("resultado",resultado)
+
+# ATIVIDADE 10
+add = 4* 20 + 3
+print("resultado",add)
+
+# ATIVIDADE 11
+sub = 15 * 2 - 7
+print("resultado",sub)
+
+# ATIVIDADE 12
+elev = 5**3
+print("resultado",elev)
+
+#ATIVIDADE 13
+med = (17 +21 +25)/3
+print("resultado",med)
+
+#ATIVIDADE 14
+mult = 11 * 2 + 7
+print("resultado",mult)
+
+# ATIVIDADE 15
+div = (15 - (3*8))/2
+print("resultado",div)
+
+# ATIVIDADE 16
+jorge = 2**10
+print("resultado", jorge)
+
+
+
+
+
+
+
+
+palavra = "isso é um teste em python"
+teste = "uhagfwehbfbwebfbwefbibwiefwbifbieuwbfbfibwefbwebhyfbuyheruf"
+comprimento = len(palavra)
+print(comprimento)
+print(len(teste))
+
+palavra = "isso é um teste"
+
+lista = list(palavra)
+print(lista)
+
+#range
+lista = list(range(1,201))
+
+print(lista)
+
+
