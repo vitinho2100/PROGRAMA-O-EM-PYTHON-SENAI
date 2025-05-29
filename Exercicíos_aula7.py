9338ced77d4a17f15864191ae747dd79bf675c32
vitinho2100 <vitor08moraes@gmail.com>
2025-05-29 15:54:54 -0300

    Add files via upload

---
+++ Exercicíos_aula7.py
@@ -0,0 +1,23 @@
+# EXERCICIO 0
+numeros = list(range(0,21,2))
+print(numeros)
+
+#EXERCICIO 1
+NUMEROS=[0,1,2,3,4,5,6,7,8,9,10]
+print(NUMEROS)
+
+#EXERCICIOS 2
+print(numeros[2])
+
+# EXERCICIOS 3
+numeros.append(9)
+print(numeros)
+
+#EXERCICIOS 4
+del numeros[5]
+print(numeros)
+
+#EXERCICIOS 5
+CARROS=['r8','nivus','onix']
+CARROS+=(NUMEROS)
+print(CARROS)
