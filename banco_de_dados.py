43023f70cdf6c4d7364efb8037440866a63db6ca
vitinho2100 <vitor08moraes@gmail.com>
2025-05-30 17:47:59 -0300

    Add files via upload

---
+++ banco_de_dados.py
@@ -0,0 +1,31 @@
+banco_dados =  {}
+
+nome1 = input('Nome')
+idade1 = input('Idade')
+
+nome2 = input('Nome')
+idade2 = input('Idade')
+
+nome3 = input('Nome')
+idade3 = input('Idade')
+
+banco_dados['nomes'] = [nome1, nome2, nome3]
+banco_dados['idades'] = [idade1, idade2, idade3]
+
+print(banco_dados)
+
+banco_dados =  {}
+
+nome1 = input('Nome')
+idade1 = input('Idade')
+
+nome2 = input('Nome')
+idade2 = input('Idade')
+
+nome3 = input('Nome')
+idade3 = input('Idade')
+
+banco_dados['nomes'] = [nome1, nome2, nome3]
+banco_dados['idades'] = [idade1, idade2, idade3]
+
+print(banco_dados)
